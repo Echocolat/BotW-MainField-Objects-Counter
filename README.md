56faf4ae53c03b021aff1269ec5765287558861d
Echocolat <98425838+Echocolat@users.noreply.github.com>
2022-07-04 11:19:41 +0200

    commit on 'master'

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ### Count in only one Map folder
 
-Before using it, take the Map Unit folders in your game files ([DLC Folder]/content/0010/Map/MainField) and paste them into the Map Units folder. Then, just launch the script. Input yes when it asks you if you want to do a one folder analysis.
+Before using it, take the Map Unit folders in your game files ([DLC Folder]/content/0010/Map/MainField) and paste them into the Map Units folder. Then, just launch the script. Input y when it asks you if you want to do a one folder analysis.
+
+### Compare in two Map folders
+
+Before using it, take the Map Unit folders in the two MainField folders you want to compare, and paste them into Comparison\Map Units 1 and Comparison\Map Units 2. Then, just launch the script. Input y when it asks you if you want to do a comparison.
--- object_count.py
+++ object_count.py
@@ -1,12 +1,13 @@
 from collections import Counter
 import oead
 import json
+import dictdiffer
 
 indexLetters = ["A-","B-","C-","D-","E-","F-","G-","H-","I-","J-"]
 indexNumbers = ["1","2","3","4","5","6","7","8"]
 allObjects = []
 
-def takeAllData():
+def takeAllDataOneFile():
     allData = {}
     for let in indexLetters:
         for numb in indexNumbers:
@@ -14,6 +15,18 @@ def takeAllData():
             sta = '_Static.smubin'
             for i in [dyn,sta]:
                 file = 'Map Units\\' + let + numb + '\\' + let + numb + i
+                with open(file,'rb') as f:
+                    allData[let + numb + i] = oead.byml.from_binary(oead.yaz0.decompress(f.read()))['Objs']
+    return allData
+
+def takeAllDataComparison(folder):
+    allData = {}
+    for let in indexLetters:
+        for numb in indexNumbers:
+            dyn = '_Dynamic.smubin'
+            sta = '_Static.smubin'
+            for i in [dyn,sta]:
+                file = 'Comparison\\' + folder + '\\' + let + numb + '\\' + let + numb + i
                 with open(file,'rb') as f:
                     allData[let + numb + i] = oead.byml.from_binary(oead.yaz0.decompress(f.read()))['Objs']
     return allData
@@ -27,7 +40,7 @@ def allMapUnits(bigData):
         oneMapUnit(bigData[mapUnit])
 
 def oneGameAnalysis(allObjects):
-    allMapUnits(takeAllData())
+    allMapUnits(takeAllDataOneFile())
     allObjects = Counter(allObjects)
     allObjectsFinal = {k: v for k, v in sorted(allObjects.items(), key=lambda item: item[1], reverse = True)}
     with open('AllObjects.json','w') as f:
@@ -37,10 +50,40 @@ def oneGameAnalysis(allObjects):
         totalObjs = totalObjs + allObjectsFinal[i]
     print('Total number of objects in MainField :',totalObjs)
 
+def comparison():
+    finalString = {}
+    allData1 = takeAllDataComparison('Map Units 1')
+    allData2 = takeAllDataComparison('Map Units 2')
+    allObjects1, allObjects2 = [], []
+    for mapUnit in allData1:
+        for object in allData1[mapUnit]:
+            allObjects1.append(object['UnitConfigName'])
+    for mapUnit in allData2:
+        for object in allData2[mapUnit]:
+            allObjects2.append(object['UnitConfigName'])
+    allObjects1 = Counter(allObjects1)
+    allObjects2 = Counter(allObjects2)
+    allObjectsFinal1 = {k: v for k, v in sorted(allObjects1.items(), key = lambda item: item[1], reverse = True)}
+    allObjectsFinal2 = {k: v for k, v in sorted(allObjects2.items(), key = lambda item: item[1], reverse = True)}
+    for diff in list(dictdiffer.diff(allObjectsFinal1,allObjectsFinal2)):
+        if diff[0] == 'remove':
+            for i in range(len(diff[2])):
+                finalString[diff[2][i][0]] = f'Found {diff[2][i][0]} in first folder and not in the second one, {diff[2][i][1]} of them.'
+        elif diff[0] == 'add':
+            for i in range(len(diff[2])):
+                finalString[diff[2][i][0]] = f'Found {diff[2][i][0]} in second folder and not in the first one, {diff[2][i][1]} of them.'
+        elif diff[0] == 'change':
+            finalString[diff[1]] = f'Found {diff[1]} in both folders. {diff[2][0]} of them in the first, {diff[2][1]} of them in the second.'
+    with open('comparison.json','w') as f:
+        f.write(json.dumps(finalString, indent = 4))
+            
+
 def main():
     if input('Do you want to do a one folder analysis ? Enter y if it\'s the case, or anything else if it\'s not : ') == "y":
         oneGameAnalysis(allObjects)
-        return 'Finished counting. Check AllObjects.json.'
+    elif input('Do you want to do a comparison between two folders ? Enter y if yes, anything else if no : ') == 'y':
+        comparison()
+    input('Press Enter to exit...')
 
 if __name__ == "__main__":
   main()
